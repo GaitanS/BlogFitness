6ec08186cb3e330651aa9e18a211088762e07ef1
Your Name <you@example.com>
2025-03-09 18:51:17 +0200

    update carduri

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -9,3 +9,14 @@ def get_item(dictionary, key):
     Usage: {{ dictionary|get_item:key }}
     """
     return dictionary.get(key)
+
+@register.filter(name='truncate_title')
+def truncate_title(value, length):
+    """
+    Truncates a string to a specified length, adding an ellipsis if it exceeds the length.
+    Usage: {{ title|truncate_title:50 }}
+    """
+    length = int(length)
+    if len(value) > length:
+        return value[:length] + "..."
+    return value
