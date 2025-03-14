6057f084e3720ed452529d8d46772d9e2f5be6bd
Your Name <you@example.com>
2025-03-14 18:52:03 +0200

    update ads.txt

--- fitness_blog/urls.py
+++ fitness_blog/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.sitemaps.views import sitemap
 from django.views.generic import TemplateView
+from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import ArticleSitemap, CategorySitemap, StaticSitemap
 
 sitemaps = {
@@ -37,6 +37,10 @@ urlpatterns = [
     ),
     path('robots.txt', TemplateView.as_view(
         template_name="robots.txt", 
+        content_type="text/plain"
+    )),
+    path('ads.txt', TemplateView.as_view(
+        template_name="ads.txt", 
         content_type="text/plain"
     )),
 ]
